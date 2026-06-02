da9a788bf8393d735bc10a68250a2374554c4dc9
xyzhuy <xyzhuy00@gmail.com>
2026-06-02 11:53:54 +0700

    v

--- train.py
+++ train.py
@@ -3,3 +3,10 @@ from utils.trainer import main
 
 if __name__ == "__main__":
     main()
+
+""" python train.py \
+  --train_data ./public/annotations/train.json \
+  --val_data ./public/annotations/val.json \
+  --image_dir ./public/train/images \
+  --val_image_dir ./public/val/images \
+  --checkpoint_dir ./models/"""
